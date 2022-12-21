2a927161122fb54d3f87317b6261f7bdaf75d769
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-12-21 16:12:49 +0100

    Bonus

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'HomePage')
+
+@section('content')
+    <section>
+        <div class="container text-center mt-4">
+            <h1>HomePage</h1>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header class="bg-dark">
+    <div class="container pt-3 d-flex justify-content-center align-items-center">
+        <nav>
+            <ul class="d-flex justify-content-end">
+                <li class="mx-5"><a href="{{ route('home') }}">Home</a></li>
+                <li class="mx-5"><a href="{{ route('comics.index') }}">Comics</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
