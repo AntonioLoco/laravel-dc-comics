d9437abc80d7b046634e3bed5c855e6f86c6663c
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-12-23 10:06:10 +0100

    Added bonus confirm delete poupop

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -21,11 +21,37 @@
 
                                 <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Vedi dettagli</a>
                                 <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-success">Modifica</a>
-                                <form action="{{ route('comics.destroy', $comic->id) }}" class="d-inline" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Elimina</button>
-                                </form>
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                    Cancellare
+                                </button>
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma Cancellazione
+                                                </h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Sei sicuro di voler cancellare il comic?</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <form action="{{ route('comics.destroy', $comic->id) }}" class="d-inline"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-danger">Elimina</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
